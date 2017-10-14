c4d840bbedb9ab1797019136322998d69c3001f2
kallivad <kallivad@gmail.com>
2017-10-14 19:53:40 +0700

    Presentation version. Dynamical loading descriptors from DB.

--- facerec/examples/build_kdev/dnn_face_recognition.bat
+++ facerec/examples/build_kdev/dnn_face_recognition.bat
@@ -1,5 +1,6 @@
 #!/bin/bash
-#./dnn_face_recognition_ex 4 0 10 ./video/test.avi
-./dnn_face_recognition_ex 10 0 10 ./video/test_I.mp4
+./dnn_face_recognition_ex 3 1 10 ./avatars/ 1
+#./dnn_face_recognition_ex 3 0 10 ./avatars/ 1 ./video/test.avi
+#./dnn_face_recognition_ex 1 0 20 ./avatars/ 1 ./video/test_I.mp4
 #./dnn_face_recognition_ex 1 0 10 dnn_descriptors.dat >dnn_face.log 2>dnn_face.log
 #./dnn_face_recognition_ex 1 0 10
--- facerec/examples/dnn_face_recognition_ex.cpp
+++ facerec/examples/dnn_face_recognition_ex.cpp
@@ -1,22 +1,6 @@
-// The contents of this file are in the public domain. See LICENSE_FOR_EXAMPLE_PROGRAMS.txt
+// 
 /*
-    This is an example illustrating the use of the deep learning tools from the dlib C++
-    Library.  In it, we will show how to do face recognition.  This example uses the
-    pretrained dlib_face_recognition_resnet_model_v1 model which is freely available from
-    the dlib web site.  This model has a 99.38% accuracy on the standard LFW face
-    recognition benchmark, which is comparable to other state-of-the-art methods for face
-    recognition as of February 2017. 
     
-    In this example, we will use dlib to do face clustering.  Included in the examples
-    folder is an image, bald_guys.jpg, which contains a bunch of photos of action movie
-    stars Vin Diesel, The Rock, Jason Statham, and Bruce Willis.   We will use dlib to
-    automatically find their faces in the image and then to automatically determine how
-    many people there are (4 in this case) as well as which faces belong to each person.
-    
-    Finally, this example uses a network with the loss_metric loss.  Therefore, if you want
-    to learn how to train your own models, or to get a general introduction to this loss
-    layer, you should read the dnn_metric_learning_ex.cpp and
-    dnn_metric_learning_on_images_ex.cpp examples.
 */
 
 #include <dlib/opencv.h>
@@ -68,7 +52,6 @@ using anet_type = loss_metric<fc_no_bias<128, avg_pool_everything<
 	max_pool<3, 3, 2, 2, relu<affine<con<32, 7, 7, 2, 2,
 	input_rgb_image_sized<150>
 	>>>>>>>>>>>>;
-
 void finish_with_error(MYSQL *con)
 {
   fprintf(stderr, "%s\n", mysql_error(con));
@@ -78,12 +61,23 @@ void finish_with_error(MYSQL *con)
 	
 int main(int argc, char** argv) try
 {	
+	if(argc <5)
+	{
+	  cout << "{error: ""Too few arguments - should be 5 min""}" << endl;
+	  return -1;
+	}
+  
 	bool debug_mode_flag = false;
-	if (argc > 4) debug_mode_flag = true;
+	if (argc > 5) debug_mode_flag = true;
+	 
+	bool gui_mode_flag;
+	if(atoi(argv[5]) == 0) gui_mode_flag = false;
+	
 
 	int id_person = atoi(argv[1]);
 	int id_camera = atoi(argv[2]);
 	int max_averaging_count = atoi(argv[3]);
+	int max_blank_frames_count = 100;
 
 	//Connecting to the MySQL database
 	cout << "MySQL client version: " << mysql_get_client_info() << endl;
@@ -91,11 +85,12 @@ int main(int argc, char** argv) try
 	MYSQL *connection = mysql_init(NULL);
 	if(connection == NULL) 
 	{
-	  cout << "Unable to create MySQL connection" << endl;
+	  cout << "{error: ""Unable to create MySQL connection""}" << endl;
 	  return -1;	  
 	}
 	
-	if (mysql_real_connect(connection, "localhost", "root", "i20cvpass", "video", 0, NULL, 0) == NULL)
+	//if (mysql_real_connect(connection, "localhost", "root", "i20cvpass", "video", 0, NULL, 0) == NULL)
+	if (mysql_real_connect(connection, "195.211.7.218", "video", "Vej7Tuf7Up", "video", 53306, NULL, 0) == NULL)
 	{
 	  fprintf(stderr, "%s\n", mysql_error(connection));
 	  mysql_close(connection);
@@ -117,18 +112,23 @@ int main(int argc, char** argv) try
 	//cv::VideoCapture  cap(id_camera);	
 	cv::VideoCapture  cap;	
 	cv::VideoCapture* capture;
-	
-	if(argc < 5)
-		    capture = new cv::VideoCapture(atoi(argv[1])); //using standart system camera 
-		else
-		    capture = new cv::VideoCapture(argv[4]);
-	
-	cap = *capture;
+			
+	/*
+	if(argc > 5)
+	{
+	  cap = cv::VideoCapture(argv[6]);
+	} 
+	else */
+	cap = cv::VideoCapture(id_camera);
+	//cap.cv::VideoCapture::set(cv::CAP_PROP_FRAME_WIDTH, 320);
+	//cap.cv::VideoCapture::set(cv::CAP_PROP_FRAME_HEIGHT, 240);
 
+	
 	cv::Mat in, frame;	
 
 	int frame_count = 0;
 	int same_count = 0;
+	int blank_count = 0;
 
 	// Display the raw image on the screen
 	image_window win;
@@ -137,11 +137,11 @@ int main(int argc, char** argv) try
 	matrix<float, 0, 1> res_descriptor;
 	matrix<rgb_pixel> dlib_avatar;
 
-	while (same_count <= max_averaging_count)
+	while (same_count < max_averaging_count && blank_count < max_blank_frames_count)
 	{
 		if (!cap.read(in))
 		{
-		  cout << "Unable to get video stream... exiting" << endl;
+		  cout << "{error: ""Unable to get video stream""}" << endl;
 		  return -1;
 		}
 
@@ -164,11 +164,12 @@ int main(int argc, char** argv) try
 			//win.add_overlay(face);
 		}
 
-		//need only one detected face
+		//need only ONE detected face
 		if (faces.size() > 1  ) 
 		{ 
 			cout << "More than one face detected!! Clear counter" << endl;
 			same_count = 0; 
+			blank_count = 0;
 			win.clear_overlay();
 			win.set_image(img);
 			continue;
@@ -177,10 +178,11 @@ int main(int argc, char** argv) try
 		//if no face detected on the frame
 		if (faces.size() == 0)
 		{
-			cout << "No faces found in image!" << endl;
+			cout << "No faces found in image! blank_count = " << blank_count << endl;
 			win.clear_overlay();
 			win.set_image(img);
 			same_count = 0;
+			blank_count++; 
 			continue;
 		}
 
@@ -198,13 +200,15 @@ int main(int argc, char** argv) try
 			res_descriptor = (res_descriptor + face_descriptors[0]) / 2;
 			if (length(res_descriptor - face_descriptors[0]) < 0.6)
 			{
-				cout << "Descriptor averaged. Same face!!! \n";
+				//cout << "Descriptor averaged. Same face!!! \n";
 				same_count++;
+				blank_count = 0;
 			}
 			else
 			{
 				cout << "Error descriptor not same - keep averaging loop \n";
 				same_count = 0;
+				blank_count = 0;
 			}
 		}
 		else
@@ -212,17 +216,26 @@ int main(int argc, char** argv) try
 			res_descriptor = face_descriptors[0];			
 		}
 
+		dlib_avatar = faces[0];
+		
 		// Finally, let's print one of the face descriptors to the screen.  
 		//cout << "Face descriptor for " << frame_count << " frame face: \n" << trans(res_descriptor) << endl;
 
-		win_faces.set_title("Detected faces");
-		win_faces.set_image(tile_images(faces));
-
-		dlib_avatar = faces[0];
+		if(gui_mode_flag)
+		{
+		  win_faces.set_title("Detected faces");
+		  win_faces.set_image(tile_images(faces));				
 		
-		win.clear_overlay();
-		win.set_image(img);
-		win.add_overlay(render_face_detections(shapes));
+		  win.clear_overlay();
+		  win.set_image(img);
+		  win.add_overlay(render_face_detections(shapes));
+		}
+	}
+	
+	if(blank_count == max_blank_frames_count)
+	{
+	  cout << "{error: ""Unable to find any face""} " << endl;
+	  return -1;
 	}
 	
 	
@@ -231,9 +244,10 @@ int main(int argc, char** argv) try
 	cv::Mat img_avatar = dlib::toMat(dlib_avatar);
 	//opencv 2.4.9 валиться тут!!!
 	cv::cvtColor(img_avatar, img_avatar, cv::COLOR_RGB2BGR);
+	std::string path(argv[4]);
 	std::string filename("face_avatar_");
 	std::string id = std::to_string(id_person);
-	filename = filename + id + std::string(".jpg");
+	filename = path + filename + id + std::string(".jpg");
 	cv::imwrite(filename, img_avatar);
 
 	//saving descriptor to a file
@@ -297,7 +311,7 @@ int main(int argc, char** argv) try
 	{
 		std::string id = std::to_string(id_person);
 		std::string filename("face_descriptor_");
-		filename = filename + id + std::string(".bin");
+		filename = filename + id + std::string(".json");
 		//in debug mode we are saving descriptor into a file
 		std::ofstream file(filename.c_str());
 		//std::ofstream file;
@@ -342,6 +356,8 @@ int main(int argc, char** argv) try
 	}
 	
 	mysql_close(connection);
+		
+	cout << "{status:""OK""}" << endl;
 	
 	return 0;
 }
